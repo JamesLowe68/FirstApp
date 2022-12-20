222df080869a23b974df4599fc7e1456f3aadfd4
Jimmy Lo <jimmylohamzah@me.com>
2022-12-20 20:33:26 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by J.K.C. LO on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
